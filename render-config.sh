a3f33bacb386aca68c4ef406527a33daab2c37d4
Anton Ptashnik <iavtomator@gmail.com>
2025-05-05 12:35:28 +0300

    add key config files for network setup
    - Wireguard configs for tunneling between a console and a remote server
    - forward all incoming server requests to a console
    - tunnel console traffic only, other passes a usual route

---
+++ render-config.sh
@@ -0,0 +1,17 @@
+export WG_LINK_SERVER_IP=10.0.0.1
+export WG_LINK_CLIENT_IP=10.0.0.2
+export WG_LINK_PREFIX=24
+export WG_ROUTING_TABLE=pstable
+export WG_PORT=51820
+export SSH_PORT=22
+
+export WG_SERVER_PRIVKEY='some key'
+export WG_SERVER_PUBKEY='some key'
+export WG_CLIENT_PRIVKEY='some key'
+export WG_CLIENT_PUBKEY='some key'
+export PS_IP='your console IP address'
+export SERVER_IP="server IP address"
+
+# render config templates based on provided values
+envsubst < $DATADIR_PATH/wg0-client-template.conf > ./wg0-client.conf
+envsubst < $DATADIR_PATH/wg0-server-template.conf > ./wg0-server.conf
