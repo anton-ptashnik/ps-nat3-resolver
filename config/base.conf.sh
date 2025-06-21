eba779478eea724e19c70ee8e91fa4e9ab7fb39f
Anton Ptashnik <iavtomator@gmail.com>
2025-06-21 23:03:44 +0300

    make network config persist between reboots

--- config/base.conf.sh
+++ config/base.conf.sh
@@ -1,7 +1,7 @@
 export DROPLET_NAME='ps-droplet'
-export WG_LINK_SERVER_IP=10.0.0.1
-export WG_LINK_CLIENT_IP=10.0.0.2
-export WG_LINK_PREFIX=24
+export WG_LINK_SERVER_IP=10.10.10.0
+export WG_LINK_CLIENT_IP=10.10.10.1
+export WG_LINK_PREFIX=31
 export WG_ROUTING_TABLE=pstable
 export WG_PORT=51820
 export SSH_PORT=22
